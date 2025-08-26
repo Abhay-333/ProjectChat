f4fbc6e3ef0e8413d85eb79ac9fd83a85124104f
Abhay-333 <abhayd0331@gmail.com>
2025-08-26 20:45:12 +0530

    Functionality of login done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./Components/LoginPage";
 import EmployeeManagement from "./Components/EmployeeManagement";
 import OrderDashboard from "./Components/OrderDashboard";
+import ForgotPassword from "./Components/ForgotPassword";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <LoginPage />
-      <EmployeeManagement />
-      <OrderDashboard />
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route
+          path="/employeeManagement"
+          element={<EmployeeManagement /> }
+        ></Route>
+        <Route path="/orderdashboard" element={<OrderDashboard />}></Route>
+        <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
+      </Routes>
     </>
   );
 }
